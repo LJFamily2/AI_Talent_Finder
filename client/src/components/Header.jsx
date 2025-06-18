490b3925206507e7ff5ea6d36580e6fc71ebfcf6
Vu Minh Ha <s3978681@rmit.edu.vn>
2025-06-18 18:40:39 +0700

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,15 +5,37 @@ function Header() {
   return (
 
     
-    <header className="bg-[#000054] h-18 overflow-visible mb-2">
+    <header className="bg-[#000054] h-18">
         <nav className="mx-auto flex max-w-7xl" aria-label="Global">
-            <div className="flex items-center gap-x-8">
-                <div className='bg-[#E60028] py-6 px-4 flex items-center justify-center'>
-                    <a href="#">
-                        <img className="h-8 w-auto" src={logo} alt=""/>
-                    </a>
+            <div className="flex items-center justify-between w-full overflow-hidden">
+                {/* Logo and website name */}
+                <div className="flex items-center">
+                    <div className='bg-[#E60028] h-21 absolute top-0 py-3 px-4 flex items-center justify-center mr-6'>
+                        <a href="/">
+                            <img className="h-8 w-auto" src={logo} alt=""/>
+                        </a>
+                    </div>
+                    
+                    {/* Space divider - decoration purpose only */}
+                    <div className="w-37 h-18"></div>
+
+                    <p className='text-lg text-white font-bold'>Talent Finder</p>
                 </div>
-                <p className='text-xl text-white font-bold mb-3'>Talent Finder</p>
+
+                {/* Navigation links */}
+                <div className="flex items-center w-max h-18 justify-end text-lg">
+                    <div className='flex items-center justify-center hover:bg-[#000032] px-8 h-full'>
+                        <a href="/verify-cv">
+                            <p className='text-white font-medium hover:underline'>CV Verification</p>
+                        </a>
+                    </div>
+
+                    <div className='flex items-center justify-center hover:bg-[#000032] px-8 h-full'>
+                        <a href="/search-tool">
+                            <p className='text-white font-medium hover:underline'>Search Tool</p>
+                        </a>
+                    </div>
+                </div>
             </div>
         </nav>
     </header>
