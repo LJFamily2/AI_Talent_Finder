87dd2bb9ade9b82fb11a20bc2bd256faf54446fe
Vu Minh Ha <s3978681@rmit.edu.vn>
2025-06-25 23:23:12 +0700

    Implement "Frequently asked questions" section

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -14,6 +14,13 @@ import googleScholarLogo from "../assets/google-scholar-logo.png";
 import updateIcon from "../assets/updated.png";
 import monitorIcon from "../assets/dashboard-monitor.png";
 import reliableIcon from "../assets/shield.png";
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
 
 function LandingPage() {
 return (
@@ -155,9 +162,58 @@ return (
         <div className="h-10"></div>
 
         <div className="bg-[#F3F4F6] w-full h-max text-[#000054] text-center py-20">
-            <h2 className="text-3xl mb-8 font-bold">Frequently asked questions</h2>
+            <h2 className="text-3xl mb-20 font-bold">Frequently asked questions</h2>
             
-            
+            <div className="w-2/3 h-max mx-auto mb-10 flex flex-col gap-5 text-justify">
+            <Accordion>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+                >
+                <Typography component="span">Is this system completely foolproof?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                Unfortunately, our system is not without error. For instance let's look at our CV Verification feature: we rely on Scopus, ORCID and OpenAlex (and Google Scholar as a last resort) to give you the verification results of candidates’ CVs, but what if the information that we want to verify falls outside of all these databases?, which means there will false negative cases. Therefore, we recommend that you use this system as a <b>supplement</b> to your own research, rather than a replacement.
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+                >
+                <Typography component="span">What type of statements in a CV can this system verify?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                Considering the complexity of the tasks and the limitation of our abilities, we currently only verify the <b>Publications</b> section of a CV. Specifically, we check whether the publications listed in the CV can be found in Scopus, ORCID or OpenAlex. If they are not found in any of these databases, we will mark them as unverifiable.
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+                >
+                <Typography component="span">Does this system include any paid features?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                No, all features of this system are <b>free to use</b>.
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+                >
+                <Typography component="span">Is there a limit on how many CVs I can upload in a day?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                No, you can upload as many CVs as you want in a day. However, we need to emphasize that CVs containing more pages take longer to process. We estimate that a CV with 18 pages may take around 2 minutes to process.
+                </AccordionDetails>
+            </Accordion>
+            </div>
         </div>
 
         <Footer />
