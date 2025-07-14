78de233a9a896106b92586f9848d256bde79c646
Vu Minh Ha <s3978681@rmit.edu.vn>
2025-07-14 12:48:51 +0700

    Indicate which page the user is in by changing header color

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import logo from '../assets/rmit-logo-white.svg';
 
 function Header() {
+  const path = window.location.pathname;
   return (
-
-    
     <header className="bg-[#000054] h-18">
         <nav className="mx-auto flex max-w-7xl" aria-label="Global">
             <div className="flex items-center justify-between w-full overflow-hidden">
@@ -15,36 +14,28 @@ function Header() {
                             <img className="h-8 w-auto" src={logo} alt=""/>
                         </a>
                     </div>
-                    
                     {/* Space divider - decoration purpose only */}
                     <div className="w-37 h-18"></div>
-
-                <a href="/landing-page">
-                    <p className='text-lg text-white font-bold'>Talent Finder</p>
-                </a>
-                    
+                    <a href="/landing-page">
+                        <p className='text-lg text-white font-bold'>Talent Finder</p>
+                    </a>
                 </div>
-
                 {/* Navigation links */}
                 <div className="flex items-center w-max h-18 justify-end text-lg">
-                    <div className='flex items-center justify-center hover:bg-[#000032] px-8 h-full'>
+                    <div className={`flex items-center justify-center px-8 h-full ${path === '/verify-cv' ? 'bg-white' : 'hover:bg-[#000032]'}`}>
                         <a href="/verify-cv">
-                            <p className='text-white font-medium hover:underline'>CV Verification</p>
+                            <p className={`font-medium hover:underline ${path === '/verify-cv' ? 'text-[#000054] font-semibold underline' : 'text-white'}`}>CV Verification</p>
                         </a>
                     </div>
-
-                    <div className='flex items-center justify-center hover:bg-[#000032] px-8 h-full'>
+                    <div className={`flex items-center justify-center px-8 h-full ${path === '/search-tool' ? 'bg-white' : 'hover:bg-[#000032]'}`}>
                         <a href="/search-tool">
-                            <p className='text-white font-medium hover:underline'>Search Tool</p>
+                            <p className={`font-medium hover:underline ${path === '/search-tool' ? 'text-[#000054] font-semibold underline' : 'text-white'}`}>Search Tool</p>
                         </a>
                     </div>
                 </div>
             </div>
         </nav>
     </header>
-
-    
-
   );
 }   
 
