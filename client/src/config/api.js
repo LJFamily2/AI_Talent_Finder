0ec5731b075647f0941a4498b9a231e9b23738be
LJFamily2 <temp@example.com>
2025-08-29 16:18:04 +0700

    Remove console log for API_BASE_URL in api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
-console.log("API_BASE_URL:", API_BASE_URL);
+
+
 // Create axios instance with default configuration
 const api = axios.create({
   baseURL: API_BASE_URL,
