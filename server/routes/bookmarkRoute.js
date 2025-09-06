966e6b30e86a143319c6f2a275278e11d56ae745
Dustin Nguyen <s3921846@rmit.edu.vn>
2025-09-06 23:31:36 +0700

    update route for bookmark/folder functions

--- server/routes/bookmarkRoute.js
+++ server/routes/bookmarkRoute.js
@@ -5,11 +5,24 @@ const {
   getBookmarks,
   addBookmarks,
   removeBookmark,
+  deleteFolder,
+  createFolder,
+  renameFolder,
+  replaceResearchersInFolder,
+  updateResearchersInFolder,
+  moveResearchersBetweenFolders,
 } = require("../controllers/bookmarkController");
 
 // Bookmark routes (all protected - require authentication)
 router.get("/", protect, getBookmarks);
 router.post("/", protect, addBookmarks);
 router.delete("/:id", protect, removeBookmark);
+router.delete("/folders/:folderName", protect, deleteFolder);
+
+router.post("/folders", protect, createFolder);
+router.patch("/folders/:folderName", protect, renameFolder);
+router.put("/folders/:folderName/researchers", protect, replaceResearchersInFolder);
+router.patch("/folders/:folderName/researchers", protect, updateResearchersInFolder);
+router.post("/folders/move", protect, moveResearchersBetweenFolders);
 
 module.exports = router;
